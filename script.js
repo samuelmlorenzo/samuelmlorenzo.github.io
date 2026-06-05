766b670ed40baba13e2e57b80c4542b5e8aa71b3
samuelmlorenzo <samuelmlorenzo@gmail.com>
2026-06-05 10:16:52 +0200

    Update script.js
    Comment out two project entries in the projects array.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const projects = [
   { name: "Cycle Optimizer", url: "https://cycleoptimizer.com/pagina-principal" },
   { name: "Syslan", url: "https://www.syslan.es/inicio" },
   { name: "Atenda by Syslan", url: "https://atenda.syslan.es/inicio" },
+  /*{ name: "TPV Electroson", url: "https://samuelmlorenzo.github.io/electroson-tpv/" },*/
   /*{ name: "The Local Guide", url: "https://samuelmlorenzo.github.io/thelocalguide-landing/" },*/
 ];
 
